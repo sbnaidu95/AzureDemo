a650be16a643bfcf98801813e476456f7246ee0b
NGS088 <NGS088@INHYDNGS902946>
2024-05-08 17:44:59 +0530

    dev

---
+++ AzureAppliction/src/main/java/com/ngs/AzureApplictionApplication.java
@@ -0,0 +1,14 @@
+package com.ngs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureApplictionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureApplictionApplication.class, args);
+		System.out.println("Appliction deployed Done..................");
+	}
+
+}
