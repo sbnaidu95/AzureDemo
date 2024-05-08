e29d080b84e71266a06661adb5e3008571a8b3b6
NGS088 <NGS088@INHYDNGS902946>
2024-05-08 10:52:33 +0530

    demo

---
+++ SpringMvcExample/src/main/java/com/ngs/bindings/Book.java
@@ -0,0 +1,18 @@
+package com.ngs.bindings;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Book {
+	
+	private Integer bookId;
+	private String  bookName;
+	private Double  bookPrice;
+	
+
+}
